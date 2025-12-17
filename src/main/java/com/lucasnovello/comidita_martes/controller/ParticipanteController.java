7d737f76899dea307b0d2b50cfdae2210fe78f60
Lucas_Novello <lucasnovello98@hotmail.com>
2025-12-17 12:09:16 -0300

    Arreglo del pom.xml al arrancar la app

--- src/main/java/com/lucasnovello/comidita_martes/controller/ParticipanteController.java
+++ src/main/java/com/lucasnovello/comidita_martes/controller/ParticipanteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/participantes")
+@RequestMapping("/api/participantes")
 public class ParticipanteController {
 
     private final IParticipanteService participanteService;
